4c1d55a63416b7ddda2a9a5a52b221cf8b33a698
Agustin <agustinphysx@gmail.com>
2023-09-21 18:22:48 -0600

    Agregando el endpoint 'medicos' en donde se imprimen en consola los datos recibidos con un POST

--- api/src/main/java/med/voll/api/controller/HelloController.java
+++ api/src/main/java/med/voll/api/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String helloworld(){
-        return "Hello world from mexico";
+        return "Hello world from ";
     }
 }

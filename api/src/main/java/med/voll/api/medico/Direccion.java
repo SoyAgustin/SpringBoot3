26f2f4b215581ce3d900d1e92ac6fad33f99746d
SoyAgustin <Agustinphysx@gmail.com>
2023-09-25 21:58:39 -0600

    Creando entidades Medico y Direccion para la persistencia con JPA

---
+++ api/src/main/java/med/voll/api/medico/Direccion.java
@@ -0,0 +1,18 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Direccion {
+    private String calle;
+    private String numero;
+    private String complemento;
+    private String ciudad;
+    private String distrito;
+}
---
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,29 @@
+package med.voll.api.medico;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+//JPA Hibernate, Persistencia con:
+@Table(name="medicos")
+@Entity(name = "Medico")
+//Lombok, genera automaticamente:
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+    private String documento;
+    @Enumerated(EnumType.STRING)
+    private Especialidad especialidad;
+    @Embedded
+    private Direccion direccion;
+}

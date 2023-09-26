fb96ff935505d7fe753351a5fd3a47d9f2224f44
Agustin <agustinphysx@gmail.com>
2023-09-26 12:47:59 -0600

    Bean validations en MedicoController, DatosDireccion,DatosRegistroMedico, errores a nivel del payload, bodys equivocados, no del servidor

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -17,7 +18,7 @@ public class MedicoController {
     @Autowired // No es recomendable usar esta anotacion aqui, ya que en pruebas unitarias puede causar problemas
     private MedicoRepository medicoRepository;
     @PostMapping
-    public void registrarMedico(@RequestBody DatosRegistroMedico datosRegistroMedico){
+    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico){
        medicoRepository.save(new Medico(datosRegistroMedico));
     }
 }
--- api/src/main/java/med/voll/api/medico/DatosRegistroMedico.java
+++ api/src/main/java/med/voll/api/medico/DatosRegistroMedico.java
@@ -1,11 +1,28 @@
 package med.voll.api.medico;
 
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.direccion.DatosDireccion;
 
 /*Un record es un objeto inmutable que solo
 * sirve para almacenar los datos para despues
 * ser leídos*/
-public record DatosRegistroMedico(String nombre, String email, String documento, Especialidad especialidad, DatosDireccion direccion) {
-
-}
+public record DatosRegistroMedico(
+        /*Estas validaciones son de la dependencia Validations
+        * NotBlank ya hace lo de NotNull pero tambie verifica que no sea ""*/
+        @NotBlank
+        String nombre,
+        @NotBlank
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d(4,6)")
+        String documento,
+        @NotNull
+        Especialidad especialidad,
+        @NotNull
+        @Valid
+        DatosDireccion direccion
+) { }

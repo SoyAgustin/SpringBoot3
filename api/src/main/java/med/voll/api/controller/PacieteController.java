b7edf7533f5411fb0d1eb8d809633093ce84af1c
Agustin <agustinphysx@gmail.com>
2023-09-26 20:01:18 -0600

    Agregando metodo listar en PacienteController

--- api/src/main/java/med/voll/api/controller/PacieteController.java
+++ api/src/main/java/med/voll/api/controller/PacieteController.java
@@ -2,10 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 
+import med.voll.api.paciente.DatosListaPaciente;
 import med.voll.api.paciente.DatosRegistroPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +24,9 @@ public class PacieteController {
     @PostMapping
     public void registrarPaciente(@RequestBody @Valid DatosRegistroPaciente datosRegistroPaciente){
        pacienteRepository.save(new Paciente(datosRegistroPaciente));
+    }
+
+    public Page<DatosListaPaciente>  listar(@PageableDefault(page=0, size=10,sort={"nombre"})Pageable paginacion){
+        return pacienteRepository.findAll(paginacion).map(DatosListaPaciente::new);
     }
 }

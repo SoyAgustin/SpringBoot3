718de71e43998bc8b64b9462eb207b9dd7d9a9d5
Agustin <agustinphysx@gmail.com>
2023-09-29 12:25:53 -0600

    Solucionnando los falsos 500 error, a 404 error con la clase TratadorDeErrores

--- api/src/main/java/med/voll/api/infra/TratadorDeErrores.java
+++ api/src/main/java/med/voll/api/infra/TratadorDeErrores.java
@@ -1,6 +1,20 @@
 package med.voll.api.infra;
 
-/*Enn esta clase se van a tratar los errores a nivel de controller, no en cada método*/
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/*En esta clase se van a tratar los errores a nivel de controller, no en cada método*/
+/*Spring AOP: Programacion orientada a aspectos*/
+@RestControllerAdvice
 public class TratadorDeErrores {
+
+    /*Cuando se identidique la exception EntityNot FoundException se va a lanzar
+    * ese método. En cualquier parte del código que se encuentre esa excepción se
+    * va a lanzar este método que retorna un 404 error*/
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarError404(){
+        return  ResponseEntity.notFound().build(); // Este es el 404 error
+    }
 }
